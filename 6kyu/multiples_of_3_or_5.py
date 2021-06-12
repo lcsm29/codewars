39623696b35304d92ebbfe74b7c05f5ff42915de
lcsm29 <lcsm29@outlook.com>
2021-06-13 03:52:52 +0900

    initial python solution for multiples of 3 or 5

---
+++ 6kyu/multiples_of_3_or_5.py
@@ -0,0 +1,20 @@
+#  Multiples of 3 or 5
+#  6 kyu
+#  https://www.codewars.com/kata/514b92a657cdc65150000006
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def solution(number):
+    def gauss_sum(num):
+        num_mults = number // num
+        if number % num == 0:
+            num_mults = number // num - 1
+        gsum = 0.5 * num_mults * (num_mults + 1)
+        return int(gsum * num)
+    return gauss_sum(3) + gauss_sum(5) - gauss_sum(15)
