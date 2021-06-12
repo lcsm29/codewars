6bf78ef61d98ad7e03c52c3da569ec66a5f0ff93
lcsm29 <lcsm29@outlook.com>
2021-06-13 03:54:33 +0900

    initial python solution for exes and ohs

---
+++ 7kyu/exes_and_ohs.py
@@ -0,0 +1,15 @@
+#  Exes and Ohs
+#  7 kyu
+#  https://www.codewars.com/kata/55908aad6620c066bc00002a
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def xo(s):
+    tmp = [c.lower() for c in s if c.lower() in 'x, o']
+    return tmp.count('x') == tmp.count('o')
