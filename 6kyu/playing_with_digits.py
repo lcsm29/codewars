e7d38bc1b0315e2d73470cbdf5bff6353d70cd6c
lcsm29 <lcsm29@outlook.com>
2021-06-13 03:51:55 +0900

    initial python solution for playing with digits

---
+++ 6kyu/playing_with_digits.py
@@ -0,0 +1,15 @@
+#  Playing with Digits
+#  6 kyu
+#  https://www.codewars.com/kata/5552101f47fc5178b1000050
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def dig_pow(n, p):
+    tmp = [int(c) ** (p + i) for i, c in enumerate(str(n))]
+    return sum(tmp) / n if sum(tmp) % n == 0 else -1
