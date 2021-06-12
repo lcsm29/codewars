d449a89f4832eee312c1f4103483c05d772d0e99
lcsm29 <lcsm29@outlook.com>
2021-06-13 03:51:00 +0900

    initial python solution for disemvowel trolls

---
+++ 7kyu/disemvowel_trolls.py
@@ -0,0 +1,14 @@
+#  Disemvowel Trolls
+#  7 kyu
+#  https://www.codewars.com/kata/52fba66badcd10859f00097e
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def disemvowel(s):
+    return ''.join([c for c in s if c.lower() not in 'aieou'])
