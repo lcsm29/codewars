cb07a3186e70ae928c079de7d0aa09022f52de33
lcsm29 <lcsm29@outlook.com>
2021-06-13 03:54:44 +0900

    initial python solution for largest 5 digit number in a series

---
+++ 7kyu/largeset_5_digit_number_in_a_series.py
@@ -0,0 +1,14 @@
+#  Largest 5 digit number in a series
+#  7 kyu
+#  https://www.codewars.com/kata/51675d17e0c1bed195000001
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def solution(digits):
+    return int(max([digits[i:i + 5] for i in range(len(digits) - 4)]))
