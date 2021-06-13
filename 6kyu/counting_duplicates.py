bb3a7db8786fd6ae6d15c6e41318cd3e8afc526a
lcsm29 <lcsm29@outlook.com>
2021-06-14 02:06:14 +0900

    best practice added counting_duplicates

--- 6kyu/counting_duplicates.py
+++ 6kyu/counting_duplicates.py
@@ -12,3 +12,17 @@
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
 def duplicate_count(text):
     return len([c for c in set(text.lower()) if text.lower().count(c) > 1])
+
+
+#    _               _                          _   _
+#   | |             | |                        | | (_)
+#   | |__   ___  ___| |_   _ __  _ __ __ _  ___| |_ _  ___ ___
+#   | '_ \ / _ \/ __| __| | '_ \| '__/ _` |/ __| __| |/ __/ _ \
+#   | |_) |  __/\__ \ |_  | |_) | | | (_| | (__| |_| | (_|  __/
+#   |_.__/ \___||___/\__| | .__/|_|  \__,_|\___|\__|_|\___\___|
+#                         | |  written by
+#                         |_|  https://codewars.com/users/tpatja
+'''tpatja
+def duplicate_count(s):
+    return len([c for c in set(s.lower()) if s.lower().count(c)>1])
+'''
