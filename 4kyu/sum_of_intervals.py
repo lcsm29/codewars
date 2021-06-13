4839968bc52f5accdd4af136f7cace4f267e2439
lcsm29 <lcsm29@outlook.com>
2021-06-14 02:05:53 +0900

    best practice added sum_of_intervals

--- 4kyu/sum_of_intervals.py
+++ 4kyu/sum_of_intervals.py
@@ -12,3 +12,22 @@
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
 def sum_of_intervals(intervals):
     return len(set([i for a, b in intervals for i in range(a, b)]))
+
+
+#    _               _                          _   _
+#   | |             | |                        | | (_)
+#   | |__   ___  ___| |_   _ __  _ __ __ _  ___| |_ _  ___ ___
+#   | '_ \ / _ \/ __| __| | '_ \| '__/ _` |/ __| __| |/ __/ _ \
+#   | |_) |  __/\__ \ |_  | |_) | | | (_| | (__| |_| | (_|  __/
+#   |_.__/ \___||___/\__| | .__/|_|  \__,_|\___|\__|_|\___\___|
+#                         | |  written by
+#                         |_|  https://codewars.com/users/Hunter_71
+'''Hunter_71
+def sum_of_intervals(intervals):
+    result = set()
+    for start, stop in intervals:
+        for x in range(start, stop):
+            result.add(x)
+            
+    return len(result)
+'''
