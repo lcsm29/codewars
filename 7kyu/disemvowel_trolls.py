c95b812a822073c1c7b63dd5369df653bed5d62f
lcsm29 <lcsm29@outlook.com>
2021-06-14 02:07:38 +0900

    best practice added disemvowel_trolls

--- 7kyu/disemvowel_trolls.py
+++ 7kyu/disemvowel_trolls.py
@@ -12,3 +12,17 @@
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
 def disemvowel(s):
     return ''.join([c for c in s if c.lower() not in 'aieou'])
+
+
+#    _               _                          _   _
+#   | |             | |                        | | (_)
+#   | |__   ___  ___| |_   _ __  _ __ __ _  ___| |_ _  ___ ___
+#   | '_ \ / _ \/ __| __| | '_ \| '__/ _` |/ __| __| |/ __/ _ \
+#   | |_) |  __/\__ \ |_  | |_) | | | (_| | (__| |_| | (_|  __/
+#   |_.__/ \___||___/\__| | .__/|_|  \__,_|\___|\__|_|\___\___|
+#                         | |  written by
+#                         |_|  https://codewars.com/users/cvk77
+'''cvk77
+def disemvowel(s):
+    return s.translate(None, "aeiouAEIOU")
+'''
