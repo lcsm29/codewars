b532ecc60f448c5ec9d1f509d4556b0fc50b8f4f
lcsm29 <lcsm29@outlook.com>
2021-06-14 02:07:57 +0900

    best practice added multiply

--- 8kyu/multiply.py
+++ 8kyu/multiply.py
@@ -26,3 +26,17 @@ if __name__ == '__main__':
     for test in basic_tests:
         case, a, b, expected = test
         print(f'{case} returns {multiply(a, b)}{", expected: {expected}" if multiply(a, b) != expected else ""}')
+
+
+#    _               _                          _   _
+#   | |             | |                        | | (_)
+#   | |__   ___  ___| |_   _ __  _ __ __ _  ___| |_ _  ___ ___
+#   | '_ \ / _ \/ __| __| | '_ \| '__/ _` |/ __| __| |/ __/ _ \
+#   | |_) |  __/\__ \ |_  | |_) | | | (_| | (__| |_| | (_|  __/
+#   |_.__/ \___||___/\__| | .__/|_|  \__,_|\___|\__|_|\___\___|
+#                         | |  written by
+#                         |_|  https://codewars.com/users/jhoffner
+'''jhoffner
+def multiply(a, b):
+    return a * b
+'''
