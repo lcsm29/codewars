929da8f9131cfc9dc9e277370cb4767661e0f415
lcsm29 <lcsm29@outlook.com>
2021-06-18 07:53:24 +0900

    Add type hint for solution()

--- 7kyu/largeset_5_digit_number_in_a_series.py
+++ 7kyu/largeset_5_digit_number_in_a_series.py
@@ -10,7 +10,7 @@
 #    888  888       `"Y88b.   888   888   888    .dP'           888'
 #    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
-def solution(digits):
+def solution(digits: str) -> int:
     return int(max([digits[i:i + 5] for i in range(len(digits) - 4)]))
 
 
