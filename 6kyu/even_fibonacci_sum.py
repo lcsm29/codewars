6b41a8625a4e7893a2081c214f6ba64ffe42bd83
lcsm29 <lcsm29@outlook.com>
2021-06-18 07:58:08 +0900

    Add type hint even_fib()

--- 6kyu/even_fibonacci_sum.py
+++ 6kyu/even_fibonacci_sum.py
@@ -10,7 +10,7 @@
 #    888  888       `"Y88b.   888   888   888    .dP'           888'
 #    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
-def even_fib(m):
+def even_fib(m: int) -> int:
     if m <= 2: return 0
     a, b = 1, 2
     sum_even = 2
