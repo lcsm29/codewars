3169cd5acb8b005ec4037be742b07f2c2c10f388
lcsm29 <lcsm29@outlook.com>
2021-06-18 07:58:18 +0900

    Add type hint duplicate_count()

--- 6kyu/counting_duplicates.py
+++ 6kyu/counting_duplicates.py
@@ -10,7 +10,7 @@
 #    888  888       `"Y88b.   888   888   888    .dP'           888'
 #    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
 #   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
-def duplicate_count(text):
+def duplicate_count(text: str) -> int:
     return len([c for c in set(text.lower()) if text.lower().count(c) > 1])
 
 
